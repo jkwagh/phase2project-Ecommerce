29fec9075f69d4c153d5f1471c6b22729e1d6822
Gvila92 <145710383+Gvila92@users.noreply.github.com>
2023-11-09 18:02:10 -0500

    Update EquipmentMarkeplace.jsx

--- phase2project-ecommerce/src/EquipmentMarkeplace.jsx
+++ phase2project-ecommerce/src/EquipmentMarkeplace.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import EquipmentList from './EquipmentList';
 import './App.css'
+import { motion } from 'framer-motion';
 
 
 function EquipmentMarkeplace({ equipment, addEquipmentToMarketplace , deleteItem}) {
@@ -36,66 +37,68 @@ function EquipmentMarkeplace({ equipment, addEquipmentToMarketplace , deleteItem
 
   return (
     <>
-    <h1>Equipment Marketplace</h1>
-    
-      <EquipmentList equipment={equipment} />
-    
-    <div className='form-container'>
-      <h2>Add New Equipment</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Name:</label>
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label>Category:</label>
-          <select
-            name="category"
-            value={formData.category}
-            onChange={handleDropDownChange}
-            defaultValue="Pick a Category">
-            {options.map((option, index) => (
-              <option key={index} value={option}>{option}</option>
-              ))
-            }
-          </select>
-        </div>
-        <div>
-          <label>Price:</label>
-          <input
-            type="number"
-            name="price"
-            value={formData.price}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label>Description:</label>
-          <textarea
-            name="description"
-            value={formData.description}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label>Image URL:</label>
-          <input
-            type="url"
-            name="image"
-            value={formData.image}
-            onChange={handleInputChange}
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      <h1>Equipment Marketplace</h1>
+
+      <EquipmentList equipment={equipmentArray} />
+
+      <div className="form-container">
+        <h2>Add New Equipment</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Name:</label>
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label>Category:</label>
+            <select
+              name="category"
+              value={formData.category}
+              onChange={handleInputChange}
+            >
+              <option value="Pick a Category" disabled>
+                Pick a Category
+              </option>
+              <option value="Strength">Strength</option>
+              <option value="Cardio">Cardio</option>
+              <option value="Mobility">Mobility</option>
+            </select>
+          </div>
+          <div>
+            <label>Price:</label>
+            <input
+              type="number"
+              name="price"
+              value={formData.price}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label>Description:</label>
+            <textarea
+              name="description"
+              value={formData.description}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label>Image URL:</label>
+            <input
+              type="url"
+              name="image"
+              value={formData.image}
+              onChange={handleInputChange}
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </>
   );
 }
 
-export default EquipmentMarkeplace;
+export default EquipmentMarketplace;
