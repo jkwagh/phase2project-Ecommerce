f52f936d2962cfe48c4f9ad008a66f35563d459f
Gvila92 <145710383+Gvila92@users.noreply.github.com>
2023-11-09 18:02:41 -0500

    Update NavBar.jsx

--- phase2project-ecommerce/src/NavBar.jsx
+++ phase2project-ecommerce/src/NavBar.jsx
@@ -1,32 +1,55 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
+import { motion } from 'framer-motion';
 
 function NavBar() {
 
   return (
-    <div> 
+    
+    <motion.div >
       <nav className="Navbar">
-        <div className="logo" onClick={() => navigate('/')}>
-          EcommerceSite
+        <div className="logo" onClick={() => navigate('/equipment-marketplace')}>
+          Do you even Lift Bay
         </div>
         <ul>
           <li className="EquipmentMarketplace">
-            <Link to="/" className='NavBarLinks'> Home </Link>
+            <Link to="/equipment-marketplace" className='NavBarLinks'>
+              <motion.div 
+              whileHover={{ scale: 1.1 }}>
+                 Home
+              </motion.div>
+            </Link>
           </li>
           <li className="Strength">
-            <Link to="/strength" className='NavBarLinks'> Strength </Link>
+            <Link to="/strength" className='NavBarLinks'>
+              <motion.div whileHover={{ scale: 1.1 }}>
+                Strength
+              </motion.div>
+            </Link>
           </li>
           <li className="Mobility">
-            <Link to="/mobility" className='NavBarLinks'> Mobility </Link>
+            <Link to="/mobility" className='NavBarLinks'>
+              <motion.div whileHover={{ scale: 1.1 }}>
+                Mobility
+              </motion.div>
+            </Link>
           </li>
           <li className="Cardio">
-            <Link to="/cardio" className='NavBarLinks'> Cardio </Link>
+            <Link to="/cardio" className='NavBarLinks'>
+              <motion.div whileHover={{ scale: 1.1 }}>
+                Cardio
+              </motion.div>
+            </Link>
           </li>
         </ul>
       </nav>
-    </div>
+    </motion.div>
   );
 }
+
+              
+            
 
 export default NavBar;
