b0d247ab512a35e7e98a08e1118fd9088543ca75
Gvila92 <145710383+Gvila92@users.noreply.github.com>
2023-11-09 18:02:24 -0500

    Update Mobility.jsx

--- phase2project-ecommerce/src/Mobility.jsx
+++ phase2project-ecommerce/src/Mobility.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import EquipmentList from './EquipmentList';
 import './App.css'
+import { motion } from 'framer-motion';
 
 
 function Mobility({ equipment }) {
@@ -11,10 +12,20 @@ function Mobility({ equipment }) {
   ;
 
   return (
+    <motion.div
+    className='Mobility'
+    initial={{ opacity: 0, scale: 0.5 }}
+    animate={{ opacity: 1, scale: 1 }}
+    transition={{ duration: 0.5 }}
+   
+ 
+    
+   >
     <div>
       <h1>Mobility Equipment</h1>
       <EquipmentList equipment={mobilityItemsData}/>
     </div>
+    </motion.div>
   );
 }
 
