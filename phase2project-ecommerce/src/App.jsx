c29203268418cc48db92e6a38a38318acead77e1
Gvila92 <145710383+Gvila92@users.noreply.github.com>
2023-11-09 18:00:32 -0500

    Update App.jsx
    transition effect

--- phase2project-ecommerce/src/App.jsx
+++ phase2project-ecommerce/src/App.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect}  from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import EquipmentMarkeplace from './EquipmentMarkeplace'
 import NavBar from './NavBar'
 import Strength from './Strength'
@@ -57,3 +57,4 @@ function App() {
 }
 
 export default App
+
